3c9806eedc818f710d27172432904f6ee9c72a4d
Sylvain Didelot <sdidelot@ddn.com>
2022-10-24 11:39:46 +0200

    fi_errno: Provide error codes specific to io_uring
    We need to differentiate io_uring error codes from other types of
    error codes, including:
    - FI_EIOURING_PREP: "io_uring operation prepared, waiting for submission"
    - FI_EIOURING_FULL: "io_uring full"
    
    Signed-off-by: Sylvain Didelot <sdidelot@ddn.com>

--- include/rdma/fi_errno.h
+++ include/rdma/fi_errno.h
@@ -194,6 +194,8 @@ enum {
 	FI_ENOAV	 = 267, /* Missing or unavailable address vector */
 	FI_EOVERRUN	 = 268, /* Queue has been overrun */
 	FI_ENORX	 = 269, /* Receiver not ready, no receive buffers available */
+	FI_EIOURING_PREP = 270, /* io_uring operation prepared, waiting for submission */
+	FI_EIOURING_FULL = 271, /* io_uring full */
 	FI_ERRNO_MAX
 };
 
--- src/fabric.c
+++ src/fabric.c
@@ -1373,6 +1373,8 @@ static const char *const errstr[] = {
 	[FI_ENOAV - FI_ERRNO_OFFSET] = "Missing or unavailable address vector",
 	[FI_EOVERRUN - FI_ERRNO_OFFSET] = "Queue has been overrun",
 	[FI_ENORX - FI_ERRNO_OFFSET] = "Receiver not ready, no receive buffers available",
+	[FI_EIOURING_PREP - FI_ERRNO_OFFSET] = "io_uring operation prepared, waiting for submission",
+	[FI_EIOURING_FULL - FI_ERRNO_OFFSET] = "io_uring full",
 };
 
 __attribute__((visibility ("default"),EXTERNALLY_VISIBLE))
